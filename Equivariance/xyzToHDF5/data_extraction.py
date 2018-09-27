10b42323c7c3f42654cca39ed059c23457ea4ffb
William Lohrmann <william.lohrmann@oxfordni.com>
2018-09-27 18:42:39 +0100

    Refactor input generation

--- Equivariance/xyzToHDF5/data_extraction.py
+++ Equivariance/xyzToHDF5/data_extraction.py
@@ -1,101 +1,102 @@
-#!/usr/local/Cluster-Apps/python/3.5.1/bin/python3
 import numpy as np
 import pandas as pd
 import pickle
+import h5py
+import pdb
 
+def to_float(s):
+    if 'e' not in s:
+        return float(s)
+    else:
+        return 0
 
-N = 10000
-df = pd.DataFrame(columns=['id', 'atom', 'x', 'y', 'z', 'U0'])
+def xyzToHDF5(N_start, N, filename, total_molecules):
+    df = pd.DataFrame(columns=['id', 'element', 'x', 'y', 'z', 'U0'])
 
-for m in range(N):
-    fname = '/home/wpl27/Data/Data/dsgdb9nsd_%06d.xyz' % (m+1)
-    with open(fname) as f:
-        for n,line in enumerate(f):
-            words = line.split()
-            if n==0:
-                na = int(line)
-            if n==1:
-                i_d = words[1]
-                U0 = words[12]
-            if n>1 and n<=na:
-                df = df.append({'id' : i_d, 'atom' : words[0], 
-                                'x' : words[1], 'y' : words[2], 'z': words[3],
-                                'U0' : U0}, ignore_index=True)
+    for m in range(N_start, N_start + N):
+        fname = './Data/dsgdb9nsd_%06d.xyz' % (m+1)
+        with open(fname) as f:
+            for n,line in enumerate(f):
+                words = line.split()
+                if n==0:
+                    na = int(line)
+                if n==1:
+                    i_d = words[1]
+                    U0 = words[12]
+                if n>1 and n<=na:
+                    df = df.append({'id' : i_d, 'element' : words[0], 
+                                    'x' : words[1], 'y' : words[2], 'z': words[3],
+                                    'U0' : U0}, ignore_index=True)
         
 
-def remove_scientific(s):
-    try:
-        float(s)
-        return s
-    except:
-        return '0'
 
-df['x'] = df['x'].apply(remove_scientific)
-df['y'] = df['y'].apply(remove_scientific)
-df['z'] = df['z'].apply(remove_scientific)
+    df['x'] = df['x'].apply(to_float)
+    df['y'] = df['y'].apply(to_float)
+    df['z'] = df['z'].apply(to_float)
 
-df['x'] = df['x'].apply(pd.to_numeric)
-df['y'] = df['y'].apply(pd.to_numeric)
-df['z'] = df['z'].apply(pd.to_numeric)
-df['id'] = df['id'].apply(pd.to_numeric)
+    df['id'] = df['id'].apply(pd.to_numeric)
 
-positions = np.array(df.iloc[:,2:5])
-ids = np.array(df['id'])
+    positions = np.array(df.iloc[:,2:5])
+    ids = np.array(df['id'])
+    elements = np.array(df['element'])
+    duplicate_U0s = np.array(df['U0'])
 
-print(np.max(np.abs(positions)))
+    for i in range(np.max(ids)):
+        molecule_positions = positions[ids==i+1]
 
-for i in range(np.max(ids)):
-    pos = positions[ids==i+1]
-    biggest = np.max(pos, axis=0)
-    smallest = np.min(pos, axis=0)
-    positions[ids==i+1] -= (biggest+smallest)/2
-
-biggest = np.max(np.abs(positions))
-print(biggest)
-
-#Specifying the input
-#20x20x20 grid
-grid_size = biggest*1.2 # Side length in Å of box
-grid_resolution = 20 # 20x20x20 boxes
-atoms = 5 # CHONF
-
-x = np.linspace(-grid_size,grid_size, grid_resolution)
+        #Center the molecule at the origin, so it fits in the smallest possible cube
+        positions[ids==i+1] -= (np.max(molecule_positions, axis=0) +
+                                np.min(molecule_positions, axis=0))/2
 
 
+    #Specifying the input
+    #20x20x20 grid
+    biggest = 10
+    grid_size = biggest*1.2 # Side length in Å of box
+    grid_points = 20 # 20x20x20 boxes
+    atoms = 5 # CHONF
+
+    x = np.linspace(-grid_size,grid_size, grid_points)
+
+    xx,yy,zz = np.meshgrid(x, x, x)
+
+    radii = np.array([0.75, 0.31, 0.66, 0.71, 0.57]) #covalent radii of elements
+    elements_dict = {'C': 0, 'H': 1, 'O': 2, 'N': 3, 'F': 4} 
+    molecule_fields = np.zeros((N,grid_points, grid_points, grid_points, atoms))
+
+    normalizations = 1/np.sqrt(2*np.pi*radii**2)
 
 
-
-xx,yy,zz = np.meshgrid(x, x, x)
-
-radii = np.array([0.75, 0.31, 0.66, 0.71, 0.57])
-atom_types = {'C': 0, 'H': 1, 'O': 2, 'N': 3, 'F': 4} 
-molecules = np.zeros((N,grid_resolution*grid_resolution*grid_resolution*atoms))
-
-
-
-for n in np.arange(N):
-    molecule = df[df['id']==(n+1)]
-    image = np.zeros((grid_resolution,grid_resolution,grid_resolution, atoms))
-    for k in np.arange(len(molecule)):
-        a = atom_types[molecule.iloc[k,1]]
-        x_pos,y_pos,z_pos = molecule.iloc[0,2:5]
-        radius = radii[a]
-        normalization = 1/np.sqrt(2*np.pi*radius**2)
-        addition = np.exp(-0.5*((xx-x_pos)**2+(yy-y_pos)**2+(zz-z_pos)**2)/radius**2)
-        image[:, :, :, a] += addition
+    for n in np.arange(N):
+        molecule_coords = positions[ids==1+n]
+        image = np.zeros((grid_points,grid_points,grid_points, atoms))
+        for k in np.arange(molecule_coords.shape[0]):
+            x_pos,y_pos,z_pos = molecule_coords[k,0], molecule_coords[k,1], molecule_coords[k,2] 
+            element = elements_dict[elements[k]]
+            normalization = normalizations[element]
+            
+            radius = radii[element]
+            addition = np.exp(-0.5*((xx-x_pos)**2+(yy-y_pos)**2+(zz-z_pos)**2)/radius**2)
+            
+            image[:, :, :, element] += addition
         
-    molecules[n] = image.flatten()
+        molecule_fields[n] = image
 
 
-U0s = np.zeros((N,1))
-for n in np.arange(N):
-    molecule = df[df['id']==(n+1)]
-    U0s[n] = float(molecule.iloc[0,5])
+    sorted_U0s, indices = np.unique(duplicate_U0s, return_index=True)
+    assert(sorted_U0s.size == N)
 
+    U0s = np.array([duplicate_U0s[l] for l in indices], dtype=np.dtype('f')).reshape((-1, 1))
 
-data = np.hstack((U0s,molecules))
+    g = h5py.File(filename, 'a')
+    dset = g.require_dataset('molecule_fields', (total_molecules, 20, 20, 20, 5), dtype=np.dtype("f"))
+    dset2 = g.require_dataset('U0',  (total_molecules, 1), dtype=np.dtype('f'))
+                    
+                    
+    dset[N_start:N_start+N] = molecule_fields
+    dset2[N_start:N_start+N] = U0s
 
-for i in range(N//100):
-    with open('./Compiled/data'+str(i)+'.pckl', 'wb') as f:
-        pickle.dump(data[100*i:100*(i+1)], f)
+    g.close()
+
+xyzToHDF5(0, 10, "data.hdf5", 102)
 
