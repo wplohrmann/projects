3b2971042a9542f918716beecd1e42e0aa97a964
William Lohrmann <wlohrmann@oni.bio>
2020-12-25 05:17:51 +0000

    Day 25 - Merry Christmas!

---
+++ aoc_2020/day25.py
@@ -0,0 +1,37 @@
+import re
+# from scipy.signal import convolve
+from copy import copy
+import matplotlib.pyplot as plt
+import numpy as np
+from itertools import product
+from collections import defaultdict, Counter
+from io_utils import from_grid, to_grid
+from linked_list import Node
+from prelude import join, fmap
+
+part_two = False
+with open("day25.txt") as f:
+    lines = fmap(int, f.readlines())
+
+def transform(n, loop_size):
+    val = 1
+    for _ in range(loop_size):
+        val = (val * n) % 20201227
+    return val
+def gen_transform(n):
+    val = 1
+    i = 1
+    while True:
+        val = (val * n) % 20201227
+        yield val, i
+        i += 1
+
+i = 1
+i_s = []
+for key, i in gen_transform(7):
+    if key in lines:
+        i_s.append(i)
+        print("Added", i)
+    if len(i_s) == 2:
+        break
+    i += 1
