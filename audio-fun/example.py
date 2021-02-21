43f62326464a4d98b02ddf490f51c3b3c7374811
William Lohrmann <wlohrmann@oni.bio>
2021-02-21 19:35:34 +0000

    Rename + take params from other file

--- audio-fun/example.py
+++ audio-fun/example.py
@@ -5,12 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wave
  
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "file.wav"
+RECORD_SECONDS = 10
+from audio import FORMAT, CHANNELS, RATE, CHUNK
  
 audio = pyaudio.PyAudio()
  
@@ -19,13 +16,13 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
                 rate=RATE, input=True,
                 frames_per_buffer=CHUNK)
 print("recording...")
-frames = []
+chunks = []
  
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+    chunk = stream.read(CHUNK)
+    chunks.append(chunk)
 print("finished recording")
-arr = np.concatenate([np.frombuffer(frame, dtype=np.int16) for frame in frames])
+data = np.concatenate([np.frombuffer(chunk, dtype=np.int16) for chunk in chunks])
  
  
 # stop Recording
@@ -38,15 +35,5 @@ if True:
     waveFile.setnchannels(CHANNELS)
     waveFile.setsampwidth(audio.get_sample_size(FORMAT))
     waveFile.setframerate(RATE)
-    waveFile.writeframes(arr)
+    waveFile.writeframes(data)
     waveFile.close()
-
-nperseg = RATE * 20e-3 # 20ms
-sample_frequencies, segment_times, stft = signal.stft(arr, fs=RATE, nperseg=RATE*20e-3)
-sample_frequencies /= 1000 # Hz -> kHz
-
-plt.imshow(np.log(np.abs(stft)), interpolation="none", extent=[segment_times[0], segment_times[-1], sample_frequencies[0], sample_frequencies[-1]], aspect="auto")
-plt.title("STFT Magnitude")
-plt.ylabel("Frequency [kHz]")
-plt.xlabel("Time [sec]")
-plt.show()
