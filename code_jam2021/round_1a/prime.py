432f5ae0c0a093a4cdb58581841a93d058d02f1f
William Lohrmann <wlohrmann@oni.bio>
2021-04-15 22:13:32 +0100

    Solve test set 2

--- code_jam2021/round_1a/prime.py
+++ code_jam2021/round_1a/prime.py
@@ -1,29 +1,32 @@
-from itertools import product
-from functools import reduce
-from copy import deepcopy
-
 def solve(primes):
     """
     primes [prime] -> number in deck
+    Max possible solution is max_sum
     """
-    get_sum = lambda hand: sum(p*n for p,n in hand.items())
-    get_prod = lambda hand: reduce(lambda x, y: x*y, (p**n for p,n in hand.items()), 1)
-    assert get_sum({1: 3, 2: 5}) == 13
-    assert get_prod({1: 3, 2: 5, 3: 1}) == 2**5 * 3
-    # All cards in sum-hand: sum too high, so reduce it gradually. If at any point we go below that's a zero.
-    # Want to get the biggest product hand.
-    # print("Solving:", primes)
+    max_sum = sum(p*n for p,n in primes.items())
+    solutions = [(1,0)] # Tuples (prod,sum)
     max_sol = 0
-    for possible_ns in product(*[range(0, n+1) for n in primes.values()]):
-        # print(possible_ns)
-        sum_hand = {p: possible_ns[i] for i, p in enumerate(primes.keys())}
-        prod_hand = {p: primes[p] - possible_ns[i] for i, p in enumerate(primes.keys())}
-        sum_n = get_sum(sum_hand)
-        prod_n = get_prod(prod_hand)
-        if sum_n == prod_n:
-            # print("Solution:", sum_hand, prod_hand)
-            if sum_n > max_sol:
-                max_sol = sum_n
+    remaining_sum = max_sum
+    for p in primes:
+        for n in range(primes[p]):
+            new_solutions = set()
+            for sol in solutions:
+                add_to_prod = (sol[0]*p, sol[1])
+                add_to_sum = (sol[0], sol[1]+p)
+                if add_to_prod[0] >= max_sum: # We need to add the rest of the cards to the sum instead, so can immediately get the end state
+                    if sol[0] == sol[1]+remaining_sum and sol[0] > max_sol:
+                        max_sol = sol[0]
+                else: # Only add to possibilities if it's still possible to add to prod hand
+                    new_solutions.add(add_to_sum)
+                    new_solutions.add(add_to_prod)
+            remaining_sum -= p
+
+            solutions = new_solutions
+
+    for sol in solutions:
+        if sol[0] == sol[1] and sol[0] > max_sol:
+            max_sol = sol[0]
+
     return max_sol
 
 # solve({2: 2, 3:1, 5:2, 7:1, 11:1})
