b9042e0b4e6de5cf67a5b18fed0a97b2a4f99dfa
William Lohrmann <wlohrmann@oni.bio>
2021-04-25 16:51:17 +0100

    Prepare templates for round 1b

---
+++ code_jam2021/round_1b/first.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def debug(*args, **kwargs):
+    pass
+debug = print
+
+def solve():
+    """
+    """
+    return 0
+if __name__=="__main__":
+    T = int(input())
+
+    for t in range(T):
+        solution = solve()
+        print("Case #"+str(t+1)+":", solution)
