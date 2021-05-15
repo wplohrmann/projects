edfc4c8081b4e1e68cda1155737a8561249558d6
William Lohrmann <wlohrmann@oni.bio>
2021-05-15 14:45:15 +0100

    Do some stuff

--- birdclef-2021/eda.py
+++ birdclef-2021/eda.py
@@ -5,23 +5,25 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
+from sklearn.decomposition import PCA
 
 def spectrogram(f):
     data, samplerate = sf.read(f)
-    nperseg = int(10e-3 * samplerate)
+    bin_width = 10e-3
+    nperseg = int(bin_width * samplerate)
     ts, fs, specgram = signal.spectrogram(data, samplerate, nperseg=nperseg)
     specgram = np.log(specgram+1e-9)
-    return specgram
+    return specgram, 10e-3
 
 def analyse(f):
     data, samplerate = sf.read(f)
     try:
         sd.play(data, samplerate)
-        clip = spectrogram(path)
+        clip, bin_width = spectrogram(f)
         clip -= clip.mean()
 
-        convolved = signal.correlate(clip, sound, mode="valid")
-        calibration = signal.correlate(clip**2, np.ones_like(sound), mode="valid")
+        convolved = signal.correlate(clip, template, mode="valid")
+        calibration = signal.correlate(clip**2, np.ones_like(template), mode="valid")
         convolved /= np.sqrt(calibration)
 
         fig, axes = plt.subplots(3, 1, sharex=True)
@@ -38,6 +40,35 @@ def analyse(f):
     finally:
         sd.stop()
 
+def extract_segments(f):
+    clip, bin_width = spectrogram(f)
+    power = clip.sum(0)
+    distance = int(1 / bin_width)
+    peaks, properties = signal.find_peaks(power, distance=distance)
+
+    segments = [clip[:,x-distance//2:x+distance//2] for x in peaks if distance//2 <= x < clip.shape[1]-distance//2]
+
+    return segments
+    # plt.plot(power)
+    # ylim = plt.ylim()
+    # plt.vlines(peaks, *ylim)
+    # plt.ylim(ylim)
+    # plt.show()
+def find_template(segments):
+    pca = PCA(1)
+    reshaped = np.stack(segments, axis=0).reshape((len(segments), -1))
+    pca.fit(reshaped)
+    for component in pca.components_:
+        plt.imshow(component.reshape(segments[0].shape))
+        plt.show()
+
+    template = pca.components_[0].reshape(segments[0].shape)
+    template -= template.mean()
+    template /= np.sqrt(np.sum(template**2))
+
+    return template
+
+
 
 
 metadata_path = "input/train_metadata.csv"
@@ -49,10 +80,17 @@ all_birds = os.listdir(short_audio_path)
 bird = "pirfly1"
 base = os.path.join(short_audio_path, bird)
 
-sound = spectrogram(os.path.join(base, os.listdir(base)[0]))[:,150:250]
-sound -= sound.mean()
-sound /= np.sqrt(np.sum(sound**2))
+# Plan to find a good example
+# Find peaks in power(t), then grab a second chunk out of each
+# Next align all the examples (max correlation), then denoise with PCA
+# Also make sure to use noise examples for denoising
+
+segments = []
+for i, f in enumerate(os.listdir(base)):
+    path = os.path.join(base, f)
+    segments.extend(extract_segments(path))
+template = find_template(segments)
 
 for i, f in enumerate(os.listdir(base)):
     path = os.path.join(base, f)
-    analyse(os.path.join(base, f))
+    analyse(path)
