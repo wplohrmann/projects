e1598a9258a59079ffe078a318622da1334edddc
William Lohrmann <wlohrmann@oni.bio>
2021-05-15 14:48:11 +0100

    Add first problem template

---
+++ code_jam2021/round_2/a.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def debug(*args, **kwargs):
+    pass
+debug = print
+
+def solve():
+    """
+    """
+    pass
+
+if __name__=="__main__":
+    T = int(input())
+
+    for t in range(T):
+        solution = solve()
+        print("Case #"+str(t+1)+":", solution)
